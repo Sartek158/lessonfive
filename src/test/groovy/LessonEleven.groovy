f9297ff9e8a5648421ba3da2dbfb86f3be7c747e
Sartek158 <sartek158@gmail.com>
2019-02-22 17:53:41 +0300

    - Добавил TestNG

--- src/test/groovy/LessonEleven.groovy
+++ src/test/groovy/LessonEleven.groovy
@@ -1,2 +1,36 @@
+import groovy.util.logging.Slf4j
+import org.testng.annotations.Test
+
+@Slf4j
+
 class LessonEleven {
+
+    @Test
+    void lessonElevenA() {
+
+        log.info("A")
+
+
+    }
+
+    @Test
+    void lessonElevenB() {
+
+        log.info("B")
+
+    }
+
+    @Test
+    void lessonElevenC() {
+
+        log.info("C")
+
+    }
+
+    @Test
+    void lessonElevenD() {
+
+        log.info("D")
+
+    }
 }
